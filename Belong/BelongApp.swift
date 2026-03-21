f86a382cc42352c94ac4e152fcd8bdbc4f51eb42
SuzumiyaHaruhi719 <lihairui335@gmail.com>
2026-03-21 02:25:03 -0400

    Initial Commit

---
+++ Belong/BelongApp.swift
@@ -0,0 +1,17 @@
+//
+//  BelongApp.swift
+//  Belong
+//
+//  Created by 666 on 2026/3/21.
+//
+
+import SwiftUI
+
+@main
+struct BelongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Belong/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Belong
+//
+//  Created by 666 on 2026/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
